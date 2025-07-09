7077915cff1dc4215761b89fa62e902c93e670bf
Carlos dos anjos <78938222+ILord1099@users.noreply.github.com>
2025-07-09 12:06:32 -0300

    Assets e TileMap

---
+++ Top down RPG/Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+
+    public float speed;
+
+
+    void Attack()
+    {
+        //attack
+    }
+
+    void Movement()
+    {
+        //Movimento
+    }
+
+    void Jump()
+    {
+        // Pulo 
+    }
+}
