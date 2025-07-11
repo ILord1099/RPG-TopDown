f3fb7cd7260ace360a4ab2ce4790fc571e25bc58
Carlos dos anjos <78938222+ILord1099@users.noreply.github.com>
2025-07-11 11:14:18 -0300

    Player movement and Animation
    animação do player ( walking e Idle) e movimentação

--- Top down RPG/Assets/Script/Player.cs
+++ Top down RPG/Assets/Script/Player.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+    
+    
     private Rigidbody2D rig;
     public float speed;
-    private Vector2 direction;
+    private Vector2 _direction;
+
+    public Vector2 direction // faz com que acesse valores em outra classe 
+    {
+        get { return _direction; }
+        set { _direction = value; }
+    }
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+       
     }
 
     private void Update()
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
         
     }
 
     private void FixedUpdate()
     {
-        rig.MovePosition(rig.position + direction * speed*Time.fixedDeltaTime);
+        rig.MovePosition(rig.position + _direction * speed*Time.fixedDeltaTime);
     }
 }
